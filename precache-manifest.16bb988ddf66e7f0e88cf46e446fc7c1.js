55a410da65bdeefe52a89ff9f86e3b978cfe0d40
Dimuth Ruwantha <dimuth@CreativeSoftware.com>
2020-05-30 21:56:35 +0530

    Updates

---
+++ precache-manifest.16bb988ddf66e7f0e88cf46e446fc7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a71c4880ec2e71c111a56c3feae27a",
+    "url": "/react-animating-tree/index.html"
+  },
+  {
+    "revision": "b5a4e06a6e6d815af2e5",
+    "url": "/react-animating-tree/static/js/2.dc0bd78f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-animating-tree/static/js/2.dc0bd78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7d8d468d531ba7c28b",
+    "url": "/react-animating-tree/static/js/main.2fe72566.chunk.js"
+  },
+  {
+    "revision": "4a728f5ef2de950bd045",
+    "url": "/react-animating-tree/static/js/runtime-main.bd29a204.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-animating-tree/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b960504da4c91c3d970bf17e511d835.js"
+  "/react-animating-tree/precache-manifest.16bb988ddf66e7f0e88cf46e446fc7c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-animating-tree/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
