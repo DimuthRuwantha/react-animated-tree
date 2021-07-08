db05eb7ab7de73fb2f1c3952fa87251a32a79c18
Dimuth Ruwantha <dimuth@CreativeSoftware.com>
2021-07-08 09:13:26 +0530

    Updates

---
+++ precache-manifest.d6c9ae83192a7c2410fbae412ed8c7dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1691d6b74390009fdcfe4059e76ea3b0",
+    "url": "/react-animating-tree/index.html"
+  },
+  {
+    "revision": "c3cdbbcfa05e8fab9a35",
+    "url": "/react-animating-tree/static/js/2.7e74585c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-animating-tree/static/js/2.7e74585c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e10285cd8205701580b",
+    "url": "/react-animating-tree/static/js/main.20ae2cec.chunk.js"
+  },
+  {
+    "revision": "dd54b58d80a7922a3c9e",
+    "url": "/react-animating-tree/static/js/runtime-main.cfcfa513.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-animating-tree/precache-manifest.8880626935359ef4286ea9ef5e8e4c6d.js"
+  "/react-animating-tree/precache-manifest.d6c9ae83192a7c2410fbae412ed8c7dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-animating-tree/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
